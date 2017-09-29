47aa8d1abe823f95cae51286d67501e9fbab029a
therusskiy <dimanrussian@gmail.com>
2017-09-29 15:28:46 +0400

    use pusher-js 4.2.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Pusher = require('pusher-js');
+var Pusher = require('pusher-js/dist/web/pusher.js');
 var pusherRedux = require('./pusher-redux');
 pusherRedux.setPusherClient(Pusher);
 module.exports = pusherRedux;
