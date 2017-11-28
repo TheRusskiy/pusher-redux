a890e597f16214ce5e763cb851be99363681266b
Nick Verwymeren <me@nickv.codes>
2017-11-28 11:31:13 -0600

    Support for react native
    Right now users will get the undefined 'document' error if they try to use this library in React Native. This should add support for React Native.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-var Pusher = require('pusher-js');
+if (typeof document === 'undefined' && typeof navigator != 'undefined' && navigator.product == 'ReactNative') {
+    // In react-native
+    var Pusher = require('pusher-js/react-native');
+}
+else {
+    var Pusher = require('pusher-js');
+}
+
+var pusherRedux = require('./lib/pusher-redux');
+pusherRedux.setPusherClient(Pusher);
+module.exports = pusherRedux;
 var pusherRedux = require('./lib/pusher-redux');
 pusherRedux.setPusherClient(Pusher);
 module.exports = pusherRedux;
